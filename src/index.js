4fdae5c87930cb3573a422d9de9c57fbbb096f81
Shubham <shubham@Shubhams-MacBook-Pro.local>
2021-03-04 15:56:37 +0530

    Add total and circulating supply routes

--- src/index.js
+++ src/index.js
@@ -12,5 +12,10 @@ app.get('/',(req, res)=>{
 app.get('/token-supply/',(req, res)=>{
     res.json(getCurrentTokenSupply(req.query.timestamp)) 
 })
-
+app.get('/circulating-supply/',(req, res)=>{
+    res.json(getCurrentTokenSupply(req.query.timestamp)) 
+})
+app.get('/total-supply/',(req, res)=>{
+    res.json(20000000) 
+})
 app.listen(PORT, ()=>console.log('Server running on PORT - ',PORT))
